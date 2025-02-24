6b2903f5b689f8cc52de1d6f3dadb3130c6cb161
vizn3r <viznertw@gmail.com>
2025-02-24 13:24:15 +0100

    platformio setup

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd([[
+let s:clip = '/mnt/c/Windows/System32/clip.exe'  " change this path according to your mount point
+if executable(s:clip)
+    augroup WSLYank
+        autocmd!
+        autocmd TextYankPost * if v:event.operator ==# 'y' | call system(s:clip, @0) | endif
+    augroup END
+endif
+
+nnoremap <C-b> :make<CR>
+]])
---
+++ lua/plugins/platformio.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "normen/vim-pio",
+  },
+  {
+    "ranjithshegde/ccls.nvim",
+    filetypes = {
+      "c",
+      "cpp",
+      "opencl",
+    },
+    lsp = {
+      use_defaults = true,
+    },
+  },
+  { "coddingtonbear/neomake-platformio" },
+}

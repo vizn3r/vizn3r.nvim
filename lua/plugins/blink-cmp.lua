058c9fc3b46cbdf49c02686afef1dcec9c5ebec4
vizn3r <viznertw@gmail.com>
2025-10-04 19:52:22 +0200

    asdf

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,8 @@
+--return {
+--  "saghen/blink.cmp",
+--  opts = {
+--    sources = {
+--      { name = "supermaven" },
+--    },
+--  },
+--}
--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,15 +1,17 @@
 return {
   "supermaven-inc/supermaven-nvim",
-  keymaps = {
-    accept_suggestion = "<Tab>",
-    clear_suggestion = "<C-]>",
-    accept_word = "<C-j>",
+  opts = {
+    --keymaps = {
+    --  accept_suggestion = "<Tab>",
+    --  clear_suggestion = "<C-]>",
+    --  accept_word = "<C-j>",
+    --},
+    --color = {
+    --  suggestion_color = "#ffffff",
+    --  cterm = 244,
+    --},
+    --log_level = "info", -- set to "off" to disable logging completely
+    disable_inline_completion = false, -- disables inline completion for use with cmp
+    --disable_keymaps = false, -- disables built in keymaps for more manual control
   },
-  color = {
-    suggestion_color = "#ffffff",
-    cterm = 244,
-  },
-  log_level = "info", -- set to "off" to disable logging completely
-  disable_inline_completion = false, -- disables inline completion for use with cmp
-  disable_keymaps = false, -- disables built in keymaps for more manual control
 }
